965039278459ce1651433f48add84dd1958f17ff
Zephania Mwando <zephaniamwando@gmail.com>
2021-04-18 23:19:27 +0300

    Project initialized

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Expense Tracker',
       theme: ThemeData(
 
         primarySwatch: Colors.deepOrange,
